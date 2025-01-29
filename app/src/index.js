2371d2e19bb2341a9e78b3bc91491f681e0fa439
Mayer MICHEL <mayer.mohib@gmail.com>
2025-01-29 14:20:35 +0100

    Suppression du dissier dist et ajout dans la liste des fichiers ignorés

--- app/src/index.js
+++ app/src/index.js
@@ -1,1 +1,28 @@
-console.log('WORLD')
+// Import de la feuille de style
+import './assets/css/style.css';
+// Import d'une image
+import imgEncore from './assets/img/Encore-backstage.webp'
+// Import des données json
+import data from './data.json'
+
+console.log('HELLO WORLD')
+const imgTag = new Image();
+imgTag.alt = 'Encore';
+imgTag.src = imgEncore;
+
+document.body.append(imgTag);
+
+// Utilisation des données
+console.log(data);
+
+const list = document.createElement('ul');
+
+for( let product of data.products ){
+    const li = document.createElement('li');
+    let content = `<h2>${product.name}</h2>`;
+    content += `<p>Prix: ${product.prix}€</p>`;
+    li.innerHTML = content;
+    list.append(li);
+}
+
+document.body.append( list );
--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,9 +1,26 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [new HtmlWebpackPlugin({
+        title: 'JS Objet avec Webpack !',
+    })],
+    module: {
+        rules: [
+        {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+        },
+        {
+            test: /\.(png|svg|jpe?g|gif|webp)$/i,
+            type: 'asset/resource',
+        },
+      ],
+    },
 };
